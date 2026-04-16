4bc7fb44fce4e696bf4b21f43e6b842c27ad7a55
Camille Zyniewicz <camille@camzyn.com>
2026-04-16 13:31:55 -0500

    feat: inquire page sticky image layout with viewport-height pinning

--- src/app/inquire/page.tsx
+++ src/app/inquire/page.tsx
@@ -8,26 +8,33 @@ export const metadata = {
 export default function InquirePage() {
   return (
     <main style={{ paddingLeft: 'var(--page-gutter)', paddingRight: 'var(--page-gutter)' }}>
-      <div className="py-16 md:py-24 flex flex-col lg:flex-row gap-12 lg:gap-20">
+
+      {/* Title */}
+      <h1 className="text-4xl md:text-5xl font-medium tracking-tight pt-16 md:pt-24 pb-8">
+        Let&rsquo;s make something.
+      </h1>
+
+      {/* 50/50 form + image */}
+      <div className="flex flex-col lg:flex-row gap-8 pb-16 md:pb-24">
 
         {/* Form */}
         <div className="w-full lg:w-1/2 shrink-0">
-          <h1 className="text-4xl md:text-5xl font-medium tracking-tight mb-8">
-            Let&rsquo;s make something.
-          </h1>
           <div className="border border-black p-8 md:p-12">
             <InquireForm />
           </div>
         </div>
 
         {/* Photo */}
-        <div className="w-full lg:w-1/2 lg:sticky lg:top-[var(--nav-height)] self-start">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/api/drive-image?id=1D1MozEzTzVi0ShK3epgPXuMYSztP-3BF"
-            alt="Blaske Studio"
-            className="w-full h-auto block object-cover"
-          />
+        <div className="w-full lg:w-1/2">
+          <div className="lg:sticky" style={{ top: 'calc(var(--nav-height) + 2rem)' }}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/api/drive-image?id=1D1MozEzTzVi0ShK3epgPXuMYSztP-3BF"
+              alt="Blaske Studio"
+              className="w-full object-cover object-bottom"
+              style={{ height: 'calc(100vh - var(--nav-height) - 4rem)' }}
+            />
+          </div>
         </div>
 
       </div>
